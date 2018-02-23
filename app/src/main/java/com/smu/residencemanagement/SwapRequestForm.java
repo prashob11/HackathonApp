d695f0ca52ab9e7bd460662b5d53350e2e1a8baa
prashob <prashob8@gmail.com>
2018-02-22 23:01:17 -0400

    SwapRequestForm, UpcomingBookings & BookFacility page created

---
+++ app/src/main/java/com/smu/residencemanagement/SwapRequestForm.java
@@ -0,0 +1,13 @@
+package com.smu.residencemanagement;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SwapRequestForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_swap_request_form);
+    }
+}
