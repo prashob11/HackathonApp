53a981cadb62084f1f8e83293df3479ab5d97dd1
prashob <prashob8@gmail.com>
2018-02-23 22:09:54 -0400

    Background Email feature integrated

--- app/src/main/java/com/smu/residencemanagement/SwapRequestForm.java
+++ app/src/main/java/com/smu/residencemanagement/SwapRequestForm.java
@@ -25,7 +25,7 @@ public class SwapRequestForm extends AppCompatActivity {
     //    addlisternerOnyesButton();
     }
 
-  /*  private void addlisternerOnyesButton() {
+  /*/  private void addlisternerOnyesButton() {
         yesButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
