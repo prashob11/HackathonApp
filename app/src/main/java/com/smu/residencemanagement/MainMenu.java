ae9088fea0ef43704a64629e94bcc7b3dd216928
prashob <prashob8@gmail.com>
2018-02-24 13:15:25 -0400

    refactoring

--- app/src/main/java/com/smu/residencemanagement/MainMenu.java
+++ app/src/main/java/com/smu/residencemanagement/MainMenu.java
@@ -32,17 +32,9 @@ public class MainMenu extends AppCompatActivity {
 
     }
 
-    /*public void goToUpcomingBookings(View view){
-
-        Intent intent = new Intent(MainMenu.this, UpcomingBookings.class);
-
-        startActivity(intent);
-
-    }
-*/
     public void goToUpcomingBookings(View view){
 
-        Intent intent = new Intent(MainMenu.this, SwapRequestForm.class);
+        Intent intent = new Intent(MainMenu.this, UpcomingBookings.class);
         startActivity(intent);
 
     }
