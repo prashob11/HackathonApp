ee1261d9638bf5858e9a4b1275427f73d55701aa
prashob <prashob8@gmail.com>
2018-02-24 00:36:44 -0400

    Email working!

--- app/src/main/java/com/smu/residencemanagement/LongOperation.java
+++ app/src/main/java/com/smu/residencemanagement/LongOperation.java
@@ -3,29 +3,21 @@ package com.smu.residencemanagement;
 
 import android.os.AsyncTask;
 import android.util.Log;
-import android.widget.EditText;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.util.Log;
-import android.widget.Toast;
 
 
 public class LongOperation extends AsyncTask<Void, Void, String> {
 
     String to = "prashob8@gmail.com";
     String from = "prashob.87@gmail.com";
+    String msgBody;
+
 
     @Override
     protected String doInBackground(Void... params) {
 
         try {
-//            String msgBody = messageBody.getText().toString();
             GMailSender sender = new GMailSender(from, "megamind");
-            sender.sendMail("Swap Request","hello from long operation",from, to);
+            sender.sendMail("Swap Request",msgBody,from, to);
         } catch (Exception e) {
 
             Log.e("error", e.getMessage(), e);
--- app/src/main/java/com/smu/residencemanagement/SwapRequestForm.java
+++ app/src/main/java/com/smu/residencemanagement/SwapRequestForm.java
@@ -1,10 +1,9 @@
 package com.smu.residencemanagement;
 
-import android.content.Intent;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.util.Log;
 import android.widget.Toast;
@@ -13,7 +12,6 @@ import android.widget.Toast;
 public class SwapRequestForm extends AppCompatActivity {
 
     EditText messageBody;
-    Button yesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,36 +19,13 @@ public class SwapRequestForm extends AppCompatActivity {
         setContentView(R.layout.activity_swap_request_form);
 
         messageBody = findViewById(R.id.editTextMessageBody);
-        yesButton = findViewById(R.id.buttonSwapYes);
-    //    addlisternerOnyesButton();
     }
-
-  /*/  private void addlisternerOnyesButton() {
-        yesButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String from = "prashob8@gmail.com";
-                String to = "prashob8@gmail.com";
-                String subject = "Swap Request";
-                String msgBody = messageBody.getText().toString();
-
-                Intent email = new Intent(Intent.ACTION_SEND);
-                email.putExtra(Intent.EXTRA_EMAIL,new String[]{to});
-                email.putExtra(Intent.EXTRA_SUBJECT, subject);
-                email.putExtra(Intent.EXTRA_TEXT, msgBody);
-                email.setType("message/rfc822");
-
-                //startActivity(Intent.createChooser(email,"Please Choose an Email Client:"));
-                startActivity(email);
-            }
-        });
-    }*/
-
 
      public void sendMail(View view) {
             try {
-
+                String msgBody = messageBody.getText().toString();
                 LongOperation l=new LongOperation();
+                l.msgBody = msgBody;
                 l.execute();  //sends the email in background
                 Toast.makeText(this, l.get(), Toast.LENGTH_SHORT).show();
             } catch (Exception e) {
