06822ba83478d7e295e58a16418dfa983f7282dc
Nitin <nitin.akash1989@gmail.com>
2018-02-25 01:40:17 -0400

    Solved problem of date in Booking

--- app/src/main/java/com/smu/residencemanagement/ReservationActivity.java
+++ app/src/main/java/com/smu/residencemanagement/ReservationActivity.java
@@ -9,17 +9,22 @@ import android.os.AsyncTask;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.util.Log;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
 import android.view.View.OnClickListener;
 import android.widget.Toast;
-import android.text.Editable;
+
 
 public class ReservationActivity extends AppCompatActivity implements
         View.OnClickListener {
@@ -34,7 +39,11 @@ public class ReservationActivity extends AppCompatActivity implements
     HttpParse httpParse = new HttpParse();
     String HttpURL = "http://dev.cs.smu.ca/~n_akash/ResidenceManagement/BookingActivity.php";
     int resId;
-
+    Intent intent;
+    String activityName;
+    HashMap<String, String> bookedEventTimes;
+    String dateOfBooking;
+    Button btnOriginal;
 
 
     @Override
@@ -42,15 +51,22 @@ public class ReservationActivity extends AppCompatActivity implements
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reservation);
-        final Intent intent = getIntent();
+        btnOriginal = (Button)findViewById(R.id.button5PM6PM);
 
-        final String activityName= intent.getStringExtra("activityType");
+        Intent intent=getIntent();
+        activityName= intent.getStringExtra("activityType");
         final String activityType = "Reserve a slot for " + intent.getStringExtra("activityType");
 
         ((TextView) findViewById(R.id.tvMessage)).setText(activityType);
         btnDatePicker = (Button) findViewById(R.id.btn_date);
         txtDate = (EditText) findViewById(R.id.in_date);
         btnDatePicker.setOnClickListener(this);
+        bookedEventTimes = (HashMap<String, String>)intent.getSerializableExtra("bookedEvents");
+        reloadPageNeeded();
+    }
+
+    public void reloadPageNeeded(){
+        final Intent intent = getIntent();
         final String userEmail = intent.getStringExtra("UserEmail");
         String[] buttonIdArray={"button6AM7AM",	"button7AM8AM",	"button8AM9AM",	"button9AM10AM",	"button10AM11AM",	"button11AM12PM",	"button12PM1PM",	"button1PM2PM",	"button2PM3PM",	"button3PM4PM",	"button4PM5PM",	"button5PM6PM",	"button6PM7PM",	"button7PM8PM",	"button8PM9PM",	"button9PM10PM",	"button10PM11PM",	"button11PM12AM"};
 
@@ -70,7 +86,7 @@ public class ReservationActivity extends AppCompatActivity implements
         });
 
         //ArrayList<String> bookedEventTimes = getIntent().getStringArrayListExtra("bookedEvents");
-        final HashMap<String, String> bookedEventTimes = (HashMap<String, String>)intent.getSerializableExtra("bookedEvents");
+
 //        Log.v("HashMapTest", bookedEventTimes.get("button6AM7AM"));
 
         //Log.d("Booked event time:","Hi ");
@@ -78,19 +94,24 @@ public class ReservationActivity extends AppCompatActivity implements
         for(final String buttonId: buttonIdArray){
             resId= getResources().getIdentifier(buttonId, "id",getPackageName());
             timeButton=(Button) findViewById(resId);
+
+            /*for(String s:bookedEventTimes.keySet())
+            {
+                Log.d("Value in keyset",s);
+            }*/
             /*Log.d("Timeslot:",buttonId);
 
             Log.d("Facility",  intent.getStringExtra("activityType"));
             Log.d("UserEmail",  userEmail);
             */
             //For swap
+            Log.d("Size before clear in refresh",Integer.toString(bookedEventTimes.size()));
             if(bookedEventTimes.keySet().contains(buttonId)){
                 findViewById(resId).setBackgroundColor(Color.DKGRAY);
                 ((Button) findViewById(resId)).setTextColor(Color.RED);
                 timeButton.setOnClickListener(new OnClickListener()
                 {
-                    public void onClick(View v)
-                    {
+                    public void onClick(View v) {
                         /*Intent intent = new Intent(ReservationActivity .this, SwapRequestForm.class);
                         startActivity(intent);*/
                         //Log.d("Inside Swap","inside swap");
@@ -98,23 +119,33 @@ public class ReservationActivity extends AppCompatActivity implements
                         Log.d("user Email address------------>",userEmail);*/
 
                         //FOR CANCEL
-                        if(bookedEventTimes.get(buttonId).equalsIgnoreCase(userEmail)){
-                            showAlert("Are you sure you want to cancel this slot?",buttonId,activityName, userEmail);
+                        if (!txtDate.getText().toString().isEmpty())
+                        {
+                            if (bookedEventTimes.get(buttonId).equalsIgnoreCase(userEmail)) {
+                                showAlert("Are you sure you want to cancel this slot?", buttonId, activityName, userEmail);
+                            } else {
+                                showAlert("Are you sure you want to ask for swapping?", buttonId, activityName, userEmail);
+                            }
                         }
-                        else{
-                            showAlert("Are you sure you want to ask for swapping?",buttonId,activityName, userEmail);
+                        else
+                        {
+                            Toast.makeText(ReservationActivity.this, "Please select date.", Toast.LENGTH_LONG).show();
                         }
-
                     }
                 });
             }
             else
             {
+                findViewById(resId).setBackgroundColor(btnOriginal.getBackground().getAlpha());
+                ((Button) findViewById(resId)).setTextColor(Color.BLACK);
                 timeButton.setOnClickListener(new OnClickListener()
                 {
                     public void onClick(View v)
                     {
-                        showAlert("Are you sure you want to book this slot?",buttonId,activityName, userEmail);
+                        if(!txtDate.getText().toString().isEmpty())
+                            showAlert("Are you sure you want to book this slot?",buttonId,activityName, userEmail);
+                        else
+                            Toast.makeText(ReservationActivity.this, "Please select date.", Toast.LENGTH_LONG).show();
                         //Intent intent = new Intent(ReservationActivity .this, BookingSummaryActivity.class);
                         //startActivity(intent);
                         //Log.d("Inside create","inside create");
@@ -123,7 +154,6 @@ public class ReservationActivity extends AppCompatActivity implements
             }
 
         }
-
     }
 
     @Override
@@ -154,7 +184,8 @@ public class ReservationActivity extends AppCompatActivity implements
     }
 
     private void showAlert(final String swapOrBook, final String buttonId, final String activity, final String userEmail) {
-        final String dateOfBooking =txtDate.getText().toString();
+        dateOfBooking =txtDate.getText().toString();
+
         final AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setMessage(swapOrBook)
                 .setPositiveButton("Yes",
@@ -164,17 +195,22 @@ public class ReservationActivity extends AppCompatActivity implements
                                 if(swapOrBook.equals("Are you sure you want to book this slot?")){
 
                                     BookingFunction(buttonId,userEmail,activity,dateOfBooking,"BOOK");
-                                     //intent = new Intent(ReservationActivity .this, BookingSummaryActivity.class);
-                                    Intent intent = getIntent();
-                                    finish();
+                                     intent = new Intent(ReservationActivity .this, BookingSummaryActivity.class);
+                                    //Intent intent = getIntent();
+                                    //finish();
                                     startActivity(intent);
                                 }
                                 else if(swapOrBook.equals("Are you sure you want to cancel this slot?")){
                                     BookingFunction(buttonId,userEmail,activity,dateOfBooking,"CANCEL");
+                                    intent = new Intent(ReservationActivity .this, BookingSummaryActivity.class);
+                                    startActivity(intent);
                                 }
                                 else
                                 {
                                     BookingFunction(buttonId,userEmail,activity,dateOfBooking,"SWAP");
+                                    intent = new Intent(ReservationActivity .this, SwapRequestForm.class);
+                                    startActivity(intent);
+
                                 }
 //                                startActivity(intent);
                             }
@@ -252,11 +288,26 @@ public class ReservationActivity extends AppCompatActivity implements
 
                 super.onPostExecute(httpResponseMsg);
 
+
+
+                //bookedEventTimes.clear();
                 progressDialog.dismiss();
-
-
                 Toast.makeText(ReservationActivity.this,httpResponseMsg.toString(), Toast.LENGTH_LONG).show();
 
+                Pattern pattern= Pattern.compile("(button\\d+(?:AM|PM)\\d+(?:AM|PM)\",\"[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\\.[a-zA-Z0-9_-]+)");
+                Matcher matcher = pattern.matcher(httpResponseMsg.toString());
+                Log.d("Size before clear",Integer.toString(bookedEventTimes.size()));
+                bookedEventTimes.clear();
+                Log.d("Size after clear",Integer.toString(bookedEventTimes.size()));
+                String[] keyValue;
+                while (matcher.find()) {
+                    keyValue= matcher.group().replace("\"","").split(",");
+
+                    bookedEventTimes.put(keyValue[0],keyValue[1]);
+                }
+
+
+                reloadPageNeeded();
             }
 
             @Override
@@ -265,6 +316,7 @@ public class ReservationActivity extends AppCompatActivity implements
                 hashMap.put("dateOfBooking",params[0]);
 
                 hashMap.put("ref","REFRESH");
+                hashMap.put("activity",activityName);
 
                 finalResult = httpParse.postRequest(hashMap, HttpURL);
 
