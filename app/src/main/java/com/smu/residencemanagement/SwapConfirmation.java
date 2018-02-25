18a3d0db760e313952c6dc3bfe8d23310118cabd
prashob <prashob8@gmail.com>
2018-02-25 07:20:09 -0400

    refactoring code

--- app/src/main/java/com/smu/residencemanagement/SwapConfirmation.java
+++ app/src/main/java/com/smu/residencemanagement/SwapConfirmation.java
@@ -1,7 +1,9 @@
 package com.smu.residencemanagement;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SwapConfirmation extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class SwapConfirmation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_swap_confirmation);
+    }
+    public void goToMainMenu(View view) {
+        Intent intent = new Intent(SwapConfirmation.this, MainMenu.class);
+        startActivity(intent);
     }
 }
